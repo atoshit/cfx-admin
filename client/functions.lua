798fec272d3bf351112fe1568fc42f1a34c07146
Atoshi <95099733+atoshit@users.noreply.github.com>
2025-05-02 07:18:59 +0200

    Delete client directory

--- client/functions.lua
+++
@@ -1,9 +0,0 @@
-local function hasPermission(permission)
-    if owner then return true end
-
-    if my_perms[permission] then return true end
-
-    return false
-end
-
-_ENV.hasPermission = hasPermission
