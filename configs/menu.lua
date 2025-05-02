48e25eca4d356da3215a2e154fa28e3cef0da13b
Atoshi <atoshit@protonmail.com>
2025-05-02 11:13:31 +0200

    RageUI dependencies streaming files and add menu configs

---
+++ configs/menu.lua
@@ -0,0 +1,19 @@
+return {
+    formatColor = "~p~", -- color of the menu
+    showHeader = true, -- if true, the header will be displayed
+    displayGlare = false, -- if true, the glare will be displayed
+    displaySubtitle = true, -- if true, the subtitle will be displayed
+    displayBackground = true, -- if true, the background will be displayed
+    displayNavigationBar = true, -- if true, the navigation bar will be displayed
+    displayInstructionalButton = true, -- if true, the instructional button will be displayed
+    displayPageCounter = true, -- if true, the page counter will be displayed
+    titleFont = 6, -- font of the titles
+    textureDictionary = "commonmenu", -- texture dictionary of the menu
+    textureName = "interaction_bgd", -- texture name of the menu
+    color = {
+        R = 0, -- red color
+        G = 0, -- green color
+        B = 0, -- blue color
+        A = 10 -- alpha color
+    }
+}
