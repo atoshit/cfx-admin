c6da2c923d35e169e1a01b203e20990ed692066d
Atoshi <atoshit@protonmail.com>
2025-05-02 10:33:10 +0200

    Remove fxmanifest.lua and clean up related directories to streamline project structure.

---
+++ fxmanifest.lua
@@ -0,0 +1,33 @@
+--- @author Atoshi (discord: atoshic17)
+--- Created at 2025-05-02
+--- Copyright (c) Atoshi - All Rights Reserved
+
+-- Resource metadata (https://docs.fivem.net/docs/scripting-reference/resource-manifest/resource-manifest/)
+fx_version 'cerulean'
+game 'gta5'
+
+lua54 'yes' -- (https://docs.fivem.net/docs/scripting-reference/resource-manifest/resource-manifest/#lua54)
+use_experimental_fxv2_oal 'yes' -- (https://docs.fivem.net/docs/scripting-reference/resource-manifest/resource-manifest/#use_experimental_fxv2_oal)
+
+author 'Atoshi'
+description 'A complete administration menu made with RageUI library with several dynamic system like rank management, player management and many other amazing features !'
+version '0.0.1'
+
+-- Resource dependency
+dependency 'oxmysql'
+
+files {
+    -- TODO: Add file not read at resource startup and read at their use (imports/)
+}
+
+shared_scripts {
+    -- TODO: Add shared scripts
+}
+
+client_scripts {
+    -- TODO: Add client scripts
+}
+
+server_scripts {
+    -- TODO: Add server scripts
+}
